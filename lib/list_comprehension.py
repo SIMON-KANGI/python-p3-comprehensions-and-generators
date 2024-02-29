3864069638c28832d6d03c0e79049232193f3842
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-02-29 07:30:47 +0300

    completed

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
 
+num_list=[3,5,7,9,7,3]
+
 def return_evens(num_list):
-    pass
+        new_list=[n for n in num_list if n % 2 == 0]
+        print(new_list)
+        
+return_evens(num_list)
+
+
+sentence_list=[]
 
 def make_exclamation(sentence_list):
-    pass
+        new_sentence=[x + "!" for x in sentence_list]
+        print(new_sentence)
+    
+make_exclamation(sentence_list)
+
